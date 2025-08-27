96014e0cded83f88cd71eafcd6c149c0bd396acd
Spudoku <calebisntshort@gmail.com>
2025-08-26 18:25:29 -0700

    Prototyping Betty's AI 60 minutes

--- Assets/Scripts/Betty/BettyAI.cs
+++ Assets/Scripts/Betty/BettyAI.cs
@@ -1,10 +1,24 @@
 using UnityEngine;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
 public class BettyAI : MonoBehaviour
 {
     // most movement stats will come from Betty's Navmesh Agent
-    [SerializeField] Transform target;
+    [SerializeField] GameObject target;
+
+    [SerializeField] bool isPursuing = false;
+    [SerializeField] bool isWandering = false;
+    [SerializeField] bool canSeeTarget = false;
+    [SerializeField] float sightDistance = 10f;
+
+    [SerializeField] LayerMask cantSeeThru;
+    [Header("Timers")]
+    [SerializeField] float targetCheckInterval = 2f;
+    [SerializeField] float blindChaseInterval = 3f;         // how long Betty can chase target after line of sight is broken
+    [SerializeField] float wanderInterval = 10f;            // how long Betty waits without a target (after blindChaseInterval runs out)
+                                                            // until she starts wandering
 
     [Header("Sounds")]
     [SerializeField] private AudioSource footStepSource;
@@ -14,36 +28,113 @@ public class BettyAI : MonoBehaviour
     NavMeshAgent agent;
     Animator animator;
 
+    [Header("Debug")]
+    [SerializeField] private float targetCheckTimer;
+    [SerializeField] private float blindChaseTimer;
+    [SerializeField] private float wanderWaitTimer;
+    [SerializeField] private Vector2 lastKnownPosition;
+
     private bool isWalking;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
 
         animator = GetComponent<Animator>();
+
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
-    }
-
-    void FixedUpdate()
-    {
-
-        Movement();
+        SetSearchingState();
+        isWalking = agent.velocity != Vector3.zero;
         animator.SetBool("isWalking", isWalking);
     }
 
-    void Movement()
-    {
-        agent.SetDestination(target.position);
 
-        isWalking = agent.velocity != Vector3.zero;
+
+    void MoveTowardsTarget(Vector2 pos)
+    {
+        if (canSeeTarget)
+        {
+            agent.SetDestination(target.transform.position);
+        }
+        else
+        {
+            agent.SetDestination(lastKnownPosition);
+        }
+
     }
 
+
+
+    #region Target Finding
+
+    // control CanSeeTarget, isPursuing and related timers
+    private void SetSearchingState()
+    {
+        canSeeTarget = CanSeeTarget();
+        if (!canSeeTarget)
+        {
+            // if can't see target, search last known location
+            blindChaseTimer -= Time.deltaTime;
+            if (blindChaseTimer > 0)
+            {
+                isPursuing = true;
+            }
+            else
+            {
+                isPursuing = false;
+                wanderWaitTimer = wanderInterval;
+            }
+        }
+        else
+        {
+            isPursuing = true;
+            lastKnownPosition = target.transform.position;
+        }
+
+        if (isPursuing)
+        {
+            MoveTowardsTarget(lastKnownPosition);
+
+        }
+        else
+        {
+            // wait until wandering
+            wanderWaitTimer -= Time.deltaTime;
+            if (wanderWaitTimer < 0)
+            {
+                lastKnownPosition = ChooseWanderPoint(transform.position);
+                wanderWaitTimer = wanderInterval;
+            }
+            else
+            {
+                lastKnownPosition = transform.position;
+            }
+        }
+    }
+
+    private Vector2 ChooseWanderPoint(Vector2 defaultPos)
+    {
+        return defaultPos;
+    }
+
+    private bool CanSeeTarget()
+    {
+        bool canSeeTarget = LineOfSight.CanSeeGO(transform, target, sightDistance, cantSeeThru);
+        Debug.Log($"[BettyAI.CanSeeTarget] trying to see target!");
+        return canSeeTarget;
+    }
+    #endregion
+
+    #region Sound Effects
     public void PlayFootStepSFX()
     {
         RandomSound.Play(footStepSource, footStepClips);
     }
+    #endregion
 }
--- Assets/Scripts/Betty/LineOfSight.cs
+++ Assets/Scripts/Betty/LineOfSight.cs
@@ -1,16 +1,89 @@
 using UnityEngine;
 
+[RequireComponent(typeof(LineRenderer))]
 public class LineOfSight : MonoBehaviour
 {
+    public float fovCone;
+    public float sightRange;
+
+    public LayerMask layerMask;
+    public bool enableLaser = true;
+
+    private LineRenderer lineRenderer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        layerMask = LayerMask.GetMask(new string[] { "Ground", "Default" });
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.positionCount = 2;
+
+        Physics2D.queriesStartInColliders = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (enableLaser)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.right, sightRange, layerMask);
+            Vector3 sightPoint;
+            if (hit.collider != null)
+            {
+                sightPoint = hit.point;
+            }
+            else
+            {
+                sightPoint = transform.position + transform.right * sightRange;
+            }
+
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, sightPoint);
+        }
+
     }
+
+    public bool CanSeeGO(GameObject go, float range)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, go.transform.position - transform.position, range, layerMask);
+        // Debug.DrawLine(transform.position, hit.point, Color.blue, 5f);
+        // Debug.Log($"[LineOfSight.CanSeeGO] trying to look at {go} at range {range}; hit.position is {hit.point}");
+        if (hit.collider != null && hit.collider.gameObject.Equals(go))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static bool CanSeeGO(Transform location, GameObject go, float range, LayerMask lm)
+    {
+        Vector2 dir = (go.transform.position - location.position).normalized;
+        // ignore own 
+        LayerMask finalMask = lm | (1 << location.gameObject.layer);
+        RaycastHit2D hit = Physics2D.Raycast(location.position, dir, range, finalMask);
+        Debug.DrawRay(location.position, dir * range, Color.red, 5f);
+        if (hit.collider != null)
+        {
+            Debug.Log($"[LineOfSight.CanSeeGO] hit {hit.collider.gameObject}!");
+            Debug.DrawLine(location.position, hit.point, Color.blue, 5f);
+        }
+        // Debug.DrawLine(location.position, go.transform.position, Color.red, 5f);
+        if (hit.collider != null && hit.collider.gameObject.Equals(go))
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public float DistanceUntilObject(Vector2 start, Vector2 dir, float range = Mathf.Infinity)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(start, dir, range);
+        if (hit.collider != null)
+        {
+            return Vector2.Distance(start, hit.point);
+        }
+        return Mathf.Infinity;
+    }
+
+
+
 }
